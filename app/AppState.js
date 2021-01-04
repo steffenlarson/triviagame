b1485bc8c5259995bc3304a84b2bcb9be3fffe57
Steffen Larson <steffman13@gmail.com>
2021-01-04 15:17:35 -0700

    deleted Value template

--- app/AppState.js
+++ app/AppState.js
@@ -1,11 +1,9 @@
 import Question from "./Models/Question.js"
-import Value from "./Models/Value.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 
 class AppState extends EventEmitter {
-  /** @type {Value[]} */
-  values = []
+
   /**@type {Question[]} */
   question = []
 }
--- app/main.js
+++ app/main.js
@@ -1,8 +1,6 @@
-import ValuesController from "./Controllers/ValuesController.js";
 import PeopleController from "./Controllers/QuestionsController.js"
 
 class App {
-  valuesController = new ValuesController();
   questionController = new PeopleController();
 }
 
